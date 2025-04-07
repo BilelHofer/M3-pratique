1d2021c919a3565347b27903f80d755c7bec5f02
BilelHofer <bilel.hofer@ik.me>
2025-04-07 12:06:47 +0200

    Ajout des classes
    - Classe état, Lot, Evennement, Operation et recette qui correspond à nos table dans la db
    - Ajout d'une classe global pour stocker les données déjà charger pour éviter de faire des appel à la db inutillement

---
+++ src/M3 Pratique/Evennement.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M3_Pratique
+{
+    /// <summary>
+    /// Classe représentant une evennement.
+    /// </summary>
+    public class Evennement
+    {
+        private int _id;
+        private string _message;
+        private DateTime _date;
+        private int _idLot;
+
+        public int Id { get => _id; set => _id = value; }
+        public string Message { get => _message; set => _message = value; }
+        public DateTime Date { get => _date; set => _date = value; }
+        public int IdLot { get => _idLot; set => _idLot = value; }
+
+        // Constructeur de la calsse evennement.
+        public Evennement(int id, string message, DateTime date, int idLot)
+        {
+            Id = id;
+            Message = message;
+            Date = date;
+            IdLot = idLot;
+        }
+    }
+}
---
+++ src/M3 Pratique/Global.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Classe Global 
+ * 
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M3_Pratique
+{
+    static class Global
+    {
+        // Liste des recettes
+        private static List<Recette> _recettes;
+
+        // Liste des opération
+        private static List<Operation> _operation;
+
+        // Liste des lots
+        private static List<Lot> _lots;
+
+        // Liste des états
+        private static List<Etat> _etats;
+
+        // Liste des évenements
+        private static List<Evennement> _evenements;
+    }
+}
