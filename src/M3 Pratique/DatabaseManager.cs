2a260ea46a2dcbe55229d6969a96b8e8b496bdd4
BilelHofer <bilel.hofer@ik.me>
2025-04-07 09:40:51 +0200

    Ajout des commentaire + db manager

---
+++ src/M3 Pratique/DatabaseManager.cs
@@ -0,0 +1,56 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Configuration;
+using System.Data;
+
+namespace M3_Pratique
+{
+    /// <summary>
+    /// Classe de gestion de la base de données.
+    /// </summary>
+
+    public class DatabaseManager
+    {
+        private static MySqlConnection _connection;
+
+        /// <summary>
+        /// Méthode pour établir la connexion à la base de données.
+        /// </summary>
+        public static void ConnectDB()
+        {
+            try
+            {
+                _connection = new MySqlConnection(ConfigurationManager.ConnectionStrings["ConnexionMySQL"].ConnectionString);
+                if (_connection.State == ConnectionState.Closed)
+                    _connection.Open();
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                _connection = null;
+            }
+        }
+
+        /// <summary>
+        /// Méthode pour récupérer la connexion à la base de données.
+        /// </summary>
+        /// <returns></returns>
+        public static MySqlConnection GetConnexion()
+        {
+            return _connection;
+        }
+
+        /// <summary>
+        /// Méthode pour fermer la connexion à la base de données.
+        /// </summary>
+        public static void CloseConnexion()
+        {
+            if (_connection != null)
+            {
+                _connection.Close();
+                _connection = null;
+            }
+        }
+    }
+}
+
--- src/M3 Pratique/Program.cs
+++ src/M3 Pratique/Program.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/**
+ * Date : 07.04.2025
+ * Auteur : Bilel Hofer, Lucas Domon
+ * Description : Programme de l'application pour la pratique du Module 3
+ * 
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
